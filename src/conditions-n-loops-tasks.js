844abde5b26c4729d7248a472d918ab11f9b8119
Yury Butskevich <bxyura@gmail.com>
2023-12-26 15:42:18 +0300

    feat: complete getIndexOf func

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -249,8 +249,15 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let res = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      res = i;
+      break;
+    }
+  }
+  return res;
 }
 
 /**
